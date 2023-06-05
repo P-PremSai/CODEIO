e16f323124ac736b04c861b918ed252a2743a507
P-PremSai <71306858+P-PremSai@users.noreply.github.com>
2023-06-05 22:47:59 +0530

    Add files via upload

---
+++ codio_intern/users/views.py
@@ -0,0 +1,94 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse   
+from django.contrib.auth import login, authenticate   
+from django.contrib.sites.shortcuts import get_current_site  
+from django.utils.encoding import force_bytes, force_str  
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode  
+from django.template.loader import render_to_string  
+from .token import account_activation_token  
+from django.core.mail import EmailMessage 
+from django.contrib.auth import get_user_model
+from .models import User
+from .forms import MyUserCreationForm
+
+def loginPage(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('codio-home')
+
+    if request.method == 'POST':
+        email = request.POST.get('email').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('codio-home')
+        else:
+            messages.error(request, 'Username OR password does not exit')
+
+    context = {'page': page}
+    return render(request, 'users/login.html', context)
+
+
+
+def register(request):
+    form = MyUserCreationForm()
+
+    if request.method == 'POST':
+        form = MyUserCreationForm(request.POST)
+        if form.is_valid():
+           # form.save()
+           # username = form.cleaned_data.get('username')
+            #messages.success(request, f'Your account is created you can login Now!')
+            #return redirect('login')
+            # save form in the memory not in database  
+            user = form.save(commit=False)  
+            user.is_active = False  
+            user.save()  
+            # to get the domain of the current site  
+            current_site = get_current_site(request)  
+            mail_subject = 'Activation link has been sent to your email id'  
+            message = render_to_string('acc_active_email.html', {  
+                'user': user,  
+                'domain': current_site.domain,  
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),  
+                'token':account_activation_token.make_token(user),  
+            })  
+            to_email = form.cleaned_data.get('email')  
+            email = EmailMessage(  
+                        mail_subject, message, to=[to_email]  
+            )  
+            email.send()  
+            return HttpResponse('Please confirm your email address to complete the registration')  
+        else:
+            messages.error(request, 'Please enter all the details correctly')
+    form = MyUserCreationForm()
+    return render(request, 'users/register.html',{'form':form})
+
+def activate(request, uidb64, token):  
+    User = get_user_model()  
+    try:  
+        uid = force_str(urlsafe_base64_decode(uidb64))  
+        user = User.objects.get(pk=uid)  
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):  
+        user = None  
+    if user is not None and account_activation_token.check_token(user, token):  
+        user.is_active = True  
+        user.save()  
+        return render(request, 'users/login.html')
+        #return HttpResponse('Thank you for your email confirmation. Now you can login your account.')  
+    else:  
+        return HttpResponse('Activation link is invalid!')  
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
